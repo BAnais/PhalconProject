902558d8271090e203089b229f2a33ae5f16f94e
BAnais <anais.banos@viacesi.fr>
2018-07-12 18:34:30 +0200

    Add : Model Articles

---
+++ app/models/HomePage/Articles.php
@@ -0,0 +1,63 @@
+<?php
+namespace HomePage;
+
+use InvalidArgumentException;
+use Phalcon\Mvc\Model;
+
+/**
+ *
+ */
+class Articles extends Model
+{
+  protected $titleArticle;
+  protected $summaryArticle;
+  protected $contentArtcile;
+  protected $publicationDate;
+
+  function getTitleArticke()
+  {
+    return $this->titelArticle;
+  }
+
+  function setTitleArticle($setTitle)
+  {
+    if(!empty($setTitle)){
+
+    }
+  }
+
+  function __construct(argument)
+  {
+    // code...
+  }
+
+  function __construct(argument)
+  {
+    // code...
+  }
+  
+  function __construct(argument)
+  {
+    // code...
+  }
+
+  function __construct(argument)
+  {
+    // code...
+  }
+
+  function __construct(argument)
+  {
+    // code...
+  }
+
+  function __construct(argument)
+  {
+    // code...
+  }
+}
+
+
+
+
+ ?>
