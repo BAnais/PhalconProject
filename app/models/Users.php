c735eeb26fe2b36ddd5afd92647c3281bcd1bb92
BAnais <anais.banos@viacesi.fr>
2018-07-13 18:04:37 +0200

    Refactor : Generate model with phalcon Command

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,29 +1,65 @@
 <?php
-namespace HomePage;
 
-use InvalidArgumentException;
-use Phalcon\Mvc\Model;
-
-/**
- *
- */
-class Users extends Model
+class Users extends \Phalcon\Mvc\Model
 {
-  protected $name;
-  protected $password;
 
-  public function __get($property)
-  {
-    if(property_exists($this, $property)){
-      return $this->$property;
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $userName;
+
+    /**
+     *
+     * @var string
+     */
+    public $userPassword;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("phalconbase");
+        $this->setSource("users");
     }
-  }
-  public function __set($property, $value)
-  {
-    if(property_exists($this, $property)){
-      $this->$property = $value;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
     }
-    return $this;
-  }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]|Users|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users|\Phalcon\Mvc\Model\ResultInterface
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
 }
-?>
