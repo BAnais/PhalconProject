f3e312f1823cd8dc332a9c0140361f2f3bfa986c
BAnais <anais.banos@viacesi.fr>
2018-07-13 13:46:14 +0200

    Add : Users model

---
+++ app/models/Users.php
@@ -0,0 +1,19 @@
+<?php
+namespace HomePage;
+
+use InvalidArgumentException;
+use Phalcon\Mvc\Model;
+
+/**
+ *
+ */
+class Users extends Model
+{
+  protected $name;
+  protected $password;
+  protected $isAdmin;
+
+
+
+}
+?>
