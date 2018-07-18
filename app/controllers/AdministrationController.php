499b747476bfd07dc485d441646d828795becf9d
BAnais <anais.banos@viacesi.fr>
2018-07-19 01:37:45 +0200

    Add : indexaction + registerAction to save new articles thanks to a form

--- app/controllers/AdministrationController.php
+++ app/controllers/AdministrationController.php
@@ -5,9 +5,43 @@ class AdministrationController extends ControllerBase
 
     public function indexAction()
     {
-
+      if(!$this->session->get('auth')){
+        return $this->dispatcher->forward(array('controller' => "signin", 'action'=> "login" ));
+      }
     }
 
+  public function registerAction()
+  {
+
+    $article = new Articles();
+
+    $success = $article->save(
+      $this->request->getPost(),
+      [
+        "title",
+        "summary",
+        "content",
+        "publicationDate",
+      ]
+    );
+
+    if($success){
+      echo "Success";
+      return $this->dispatcher->forward(array('controller' => "articles", 'action'=> "index" ));
+
+    }else{
+      echo "Sorry the following problems were generated : ";
+      return $this->dispatcher->forward(array('controller' => "articles", 'action'=> "index" ));
+
+
+      $messages = $article->getMessages();
+
+      foreach ($messages as $message) {
+        echo $message->getMessage(), "<br/>";
+      }
+    }
+    $this->view->disable();
+  }
 
 }
  ?>
