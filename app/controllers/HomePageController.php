90fef55bc0a31ab3064198a2e424bf019f9177df
BAnais <anais.banos@viacesi.fr>
2018-07-19 01:32:36 +0200

    Add : addCSS css and BS + addJS BS and Jquery

--- app/controllers/HomePageController.php
+++ app/controllers/HomePageController.php
@@ -5,6 +5,12 @@ class HomePageController extends ControllerBase
 
     public function indexAction()
     {
+      $this->assets->addCss('css/style.css',false);
+      $this->assets->addCss('css/bootstrap.css',false);
+      $this->assets->addJs('js/jquery.js',false);
+      $this->assets->addJs('js/bootstrap.js',false);
+
+
       $this->view->articles = Articles::find([
         "order"=> "publicationDate DESC",
       ]);
@@ -12,12 +18,5 @@ class HomePageController extends ControllerBase
     }
 
 }
-
-
-
-
-
-
-
 
  ?>
