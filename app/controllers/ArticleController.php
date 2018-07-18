231cceff04b5d1a22ce3e0919e8baf27633f8b3f
BAnais <anais.banos@viacesi.fr>
2018-07-19 01:31:54 +0200

    Refactor : index action findfirst by $id

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -3,9 +3,9 @@
 class ArticleController extends ControllerBase
 {
 
-    public function indexAction()
+    public function indexAction($id)
     {
-
+      $this->view->articles = Articles::findFirst($id);
     }
 }
  ?>
