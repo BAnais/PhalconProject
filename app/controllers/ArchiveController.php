aa68361aa80322a08877cdd87303d1618accea24
BAnais <anais.banos@gmail.com>
2018-07-20 00:55:25 +0200

    Add : redirect if auth

--- app/controllers/ArchiveController.php
+++ app/controllers/ArchiveController.php
@@ -5,6 +5,9 @@ class ArchiveController extends ControllerBase
 
     public function indexAction()
     {
+      if($this->session->get('auth')){
+        return $this->response->redirect("articles")->send();
+      }
       $this->view->articles = Articles::find([
         "order"=> "publicationDate DESC",
       ]);
