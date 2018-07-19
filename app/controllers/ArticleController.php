45580b87f3472a0245e73c72bb519291c8cb83e4
BAnais <anais.banos@gmail.com>
2018-07-20 00:55:11 +0200

    Add : redirect if not auth

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -5,6 +5,9 @@ class ArticleController extends ControllerBase
 
     public function indexAction($id)
     {
+      if($this->session->get('auth')){
+        return $this->response->redirect("articles")->send();
+      }
       $this->view->articles = Articles::findFirst($id);
     }
 }
